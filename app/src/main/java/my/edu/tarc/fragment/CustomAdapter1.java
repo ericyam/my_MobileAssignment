5c7e0ff082126005e97f1ef648971c29148589c5
anson-GH <ansonooi66@hotmail.com>
2016-07-31 22:36:37 +0800

    upload

--- app/src/main/java/my/edu/tarc/fragment/CustomAdapter1.java
+++ app/src/main/java/my/edu/tarc/fragment/CustomAdapter1.java
@@ -1,32 +1,32 @@
 package my.edu.tarc.fragment;
 
-        import android.app.Activity;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.View.OnClickListener;
-        import android.view.ViewGroup;
-        import android.widget.BaseAdapter;
-        import android.widget.ImageView;
-        import android.widget.TextView;
-        import android.widget.Toast;
-public class CustomAdapter extends BaseAdapter{
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomAdapter1 extends BaseAdapter{
     String [] result, result2;
     Context context;
     int [] imageId;
     Intent intent;
 
     private static LayoutInflater inflater=null;
-    public CustomAdapter(HomeActivity homeActivity, String[] prgmNameList, int[] prgmImages, String[] subList) {
+
+    public CustomAdapter1(AboutActivity aboutActivity, String[] prgmNameList, int[] prgmImages, String[] subList) {
         // TODO Auto-generated constructor stub
         result=prgmNameList;
-        context=homeActivity;
+        context=aboutActivity;
         imageId=prgmImages;
         result2=subList;
         inflater = ( LayoutInflater )context.
                 getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
+
 
     @Override
     public int getCount() {
@@ -56,7 +56,7 @@ public class CustomAdapter extends BaseAdapter{
         // TODO Auto-generated method stub
         Holder holder=new Holder();
         View rowView;
-        rowView = inflater.inflate(R.layout.navigation, null);
+        rowView = inflater.inflate(R.layout.navigation1, null);
         //set Text and Listener is here <---------------------
         holder.tv=(TextView) rowView.findViewById(R.id.textView1);
         holder.img=(ImageView) rowView.findViewById(R.id.imageView1);
@@ -64,6 +64,7 @@ public class CustomAdapter extends BaseAdapter{
         holder.tv.setText(result[position]);
         holder.img.setImageResource(imageId[position]);
         holder.tv2.setText(result2[position]);
+
 
         return rowView;
     }

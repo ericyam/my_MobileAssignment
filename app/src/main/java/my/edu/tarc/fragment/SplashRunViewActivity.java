82405c2040cc324c0e52a674b82be97296fe05d5
ericyam <coolyam_94@hotmail.com>
2016-08-24 16:12:29 +0800

    21/8 anson

--- app/src/main/java/my/edu/tarc/fragment/SplashRunViewActivity.java
+++ app/src/main/java/my/edu/tarc/fragment/SplashRunViewActivity.java
@@ -14,22 +14,24 @@ public class SplashRunViewActivity extends AppCompatActivity {
     ImageView imageMict;
     AnimationDrawable animat;
         /** Duration of wait **/
-        private final int SPLASH_DISPLAY_LENGTH = 1000;
+        private final int SPLASH_DISPLAY_LENGTH = 3000;
 
         /** Called when the activity is first created. */
         @Override
         public void onCreate(Bundle icicle) {
             super.onCreate(icicle);
             setContentView(R.layout.activity_splash_run_view);
+            getSupportActionBar().hide();
             imageMict = (ImageView)findViewById(R.id.splashscreen);
             imageMict.setBackgroundResource(R.drawable.animatesplash);
+            animat = (AnimationDrawable) imageMict.getBackground();
+            animat.run();
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
             new Handler().postDelayed(new Runnable(){
                 @Override
                 public void run() {
-                  //  animat.start();
                 /* Create an Intent that will start the Menu-Activity. */
                     Intent mainIntent = new Intent(SplashRunViewActivity.this,HomeActivity.class);
                     SplashRunViewActivity.this.startActivity(mainIntent);
